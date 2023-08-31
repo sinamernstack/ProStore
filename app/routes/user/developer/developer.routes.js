53ac7ea6a129735a9839155b9ba9ed8212f5356c
$imba <107941962+sinaun4given@users.noreply.github.com>
2023-08-31 16:38:08 +0330

    update swagger

---
+++ app/routes/user/developer/developer.routes.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const { randomNumberGenerator } = require("../../../utils/functions");
+/**
+ * @swagger
+ *  tags:
+ *      name: Developer-Route
+ *      description: Developers Utils
+ */
+
+/**
+ /**
+ * @swagger
+ *  /developer/password-hash/{password}:
+ *   get:
+ *       tags : [Developer-Route] 
+ *       summary: hash data with bcrypet
+ *       description: fresh token
+ *       parameters:
+ *       -    name: password
+ *            description: enter pass for hash
+ *            in: path
+ *            required: true
+ *            type: string 
+ *       
+ *       responses:
+ *           200:
+ *               description: Success
+ *         
+ *  
+ */ 
+router.get("/password-hash/:password", (req, res, next) => {
+  const { password } = req.params;
+  const salt = bcrypt.genSaltSync(10);
+  return res.send(bcrypt.hashSync(password, salt));
+});
+
+
+ /**
+ * @swagger
+ *  /developer/random-number:
+ *   get:
+ *       tags : [Developer-Route] 
+ *       summary: get random number
+ *       description: ...
+ *       responses:
+ *           200:
+ *               description: Success
+ *         
+ *  
+ */ 
+ router.get("/random-number", (req, res, next) => {
+ 
+  return res.send(randomNumberGenerator().toString());
+});
+module.exports = { DeveloperRoutes: router };
---
+++ app/utils/init_redis.js
@@ -0,0 +1,17 @@
+const redisDB = require("redis");
+const redisClient = redisDB.createClient();
+redisClient.connect();
+redisClient.on("connect", () => {
+  console.log("connect to redis");
+});
+redisClient.on("error", (err) => {
+  console.log("redis error : ", err.message);
+});
+redisClient.on("ready", () => {
+  console.log("connected to redis and ready to use...");
+});
+redisClient.on("end", () => {
+  console.log("disconnected from redis....");
+});
+module.exports = { redisClient };
+  
